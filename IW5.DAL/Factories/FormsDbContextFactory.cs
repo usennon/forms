346a229384a2ce9ed0e4108c3598cb4b7b6d65ff
Albert Popov <govman32@gmail.com>
2024-10-24 15:11:49 +0200

    fix: minor fix

---
+++ IW5.DAL/Factories/FormsDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace IW5.DAL.Factories
+{
+    public class FormsDbContextFactory : IDesignTimeDbContextFactory<FormsDbContext>
+    {
+        public FormsDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets<FormsDbContextFactory>(optional: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<FormsDbContext>();
+            optionsBuilder.UseSqlServer("Server=tcp:sqldb-iw5-2024-team-xpopov10.database.windows.net,1433;Initial Catalog=db-iw5-2024-team-xpopov10;Persist Security Info=False;User ID=xpopov10;Password=/Automation#567*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=100;");
+            return new FormsDbContext(optionsBuilder.Options);
+        }
+    }
+}
