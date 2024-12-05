7f5eb7cfd8f5fa6ac6409d0bc717901e64156509
Vladyslav Malashchuk <xmalas04@vutbr.cz>
2024-12-05 15:11:01 +0100

    fix : Added more fields for list of forms(Author,Questions)

--- IW5.BL.Models/ListModels/FormListModel.cs
+++ IW5.BL.Models/ListModels/FormListModel.cs
@@ -1,4 +1,5 @@
 ﻿using IW5.BL.Models.ListModels;
+using IW5.Models.Entities;
 
 namespace IW5.BL.Models.ListModels
 {
@@ -8,5 +9,7 @@ namespace IW5.BL.Models.ListModels
         public DateTime CreatedAt { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public User Author { get; set; }
+        public ICollection<Question> Questions { get; set; }
     }
 }
