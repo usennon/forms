8e85e479cef0fa2cd4454844050ab00810492047
Vladyslav Malashchuk <xmalas04@vutbr.cz>
2024-12-05 18:40:59 +0100

    fix : User Map

--- IW5.BL.Models/ListModels/UserListModel.cs
+++ IW5.BL.Models/ListModels/UserListModel.cs
@@ -1,10 +1,32 @@
 ﻿using IW5.BL.Models.ListModels;
+using IW5.Common.Enums;
+using IW5.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace IW5.BL.Models.ListModels
 {
     public class UserListModel : ListModelBase
     {
-        public string UserName { get; set; }
-        public string PhotoUrl { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        [Url]
+        public string? PhotoUrl { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(256)]
+        public string Email { get; set; }
+
+        [Phone]
+        [MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+
     }
 }
