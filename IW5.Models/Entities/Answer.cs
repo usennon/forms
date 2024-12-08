6016e8526823deac6c2386d6f65cad1c79f21024
Vladyslav Malashchuk <xmalas04@vutbr.cz>
2024-12-08 17:46:47 +0100

    feat : Answer Entity

---
+++ IW5.Models/Entities/Answer.cs
@@ -0,0 +1,20 @@
+﻿using IW5.Models.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Answer : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+    [ForeignKey("Form")]
+    public Guid FormId { get; set; }
+    public Form Form { get; set; }
+    [ForeignKey("Question")]
+    public Guid QuestionId { get; set; }
+    public Question Question { get; set; }
+
+    public string AnswerText { get; set; }
+    [ForeignKey("AnswerOption")]
+    public Guid? AnswerOptionId { get; set; }
+
+    public Option AnswerOption { get; set; }
+}
